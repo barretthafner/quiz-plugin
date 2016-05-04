5e725d0af65cfe22459a25c68291d14718a88f1e
Barrett Hafner <barrett@hafnerindustries.com>
2016-05-03 17:17:01 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $('#quiz1').quiz({
+    quizSeed: quiz1,
+    templateSelector: '#quiz-template'
+  });
+
+//  $('#quiz2').quiz({
+//    quizSeed: quiz1,
+//    templateSelector: '#quiz-template'
+//  });
+
+});
